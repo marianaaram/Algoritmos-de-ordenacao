6181e6c014aa645f804b453b2ce99e5417b5e368
Mariana <marianaaram8@gmail.com>
2022-11-30 20:19:39 -0300

    first commit

---
+++ src/main/Bubble_sort.java
@@ -0,0 +1,44 @@
+package main;
+
+public class Bubble_sort {
+
+	//Função troca 
+		public static void Swap(int vet[],int i, int j) {
+			int aux = vet[i];
+			vet[i]= vet[j];
+			vet[j]=aux;
+		}
+	  
+	  
+	  //Função bubblesort
+		public static void Bubble (int[]vet) { 
+			for(int i=vet.length-1;i>0;i--) {
+	        	for(int j=0;j<i;j++) {
+	        		if(vet[j]>vet[j+1]) {
+	        			Swap(vet,j,j+1);
+	        		} 
+	        	}		
+			}
+		}
+	
+
+	//MAIN
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Vetor de teste 
+		int [] vetor = new int[] {3,5,7,6};
+				
+		//chando o procedimento
+		Bubble(vetor);
+				
+		//Saida do vetor
+		System.out.print("Vetor ordenado: \n");
+		for(int i=0;i<vetor.length;i++) {
+			System.out.print(vetor[i]+"\n");
+		}		
+				
+
+	}
+
+}
